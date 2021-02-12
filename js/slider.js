b0ff01085ab416da396d828762b99fb3ef73f931
Alapan Bagchi <artifexfreelancer@gmail.com>
2021-02-12 06:08:39 -0800

    mozilla compatibility added

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,38 @@
-new Splide( '#slidewrapper', {
-	perPage: 3,
-	rewind : true,
-} ).mount();
+let slideWidth=document.getElementsByClassName('slides')[0].clientWidth
+let shift=0
+let count=0
+let count2=0
+console.log(slideWidth)
+
+document.getElementById('next').addEventListener("click",()=>{
+  count++
+  console.log(count,[...document.getElementsByClassName('1')].length)
+    if(count<[...document.getElementsByClassName('1')].length)
+    {
+      shift-=(slideWidth+100)
+      document.getElementById('slidewrapper').style.marginLeft=`${shift}px`
+      
+    }
+})
+
+
+
+
+document.getElementById('previous').addEventListener("click",()=>{
+  count--
+  if(count==0)
+  {
+  }
+  else
+  {
+  shift+=slideWidth+100
+  document.getElementById('slidewrapper').style.marginLeft=`${shift}px`
+
+  }
+
+})
+
+
+
+
+
