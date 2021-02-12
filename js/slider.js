a94d9df2760f12907353cf7d9782e285ceef9445
Alapan Bagchi <artifexfreelancer@gmail.com>
2021-02-12 01:47:43 -0800

    contacts added

--- js/slider.js
+++ js/slider.js
@@ -1,23 +1,4 @@
-
-let next = [...document.querySelectorAll('#next')]
-let previous = [...document.querySelectorAll('#previous')];
-let sliders = [...document.querySelectorAll('.slidewrapper')]
-
-
-    new Glider(sliders[0], {
-        slidesToShow: 1,
-        draggable:true,
-        arrows: {
-          prev: `#previous`,
-          next: `#next`
-        }
-      });
-
-      new Glider(sliders[1], {
-        slidesToShow: 1,
-        draggable:true,
-        arrows: {
-          prev: `#previous2`,
-          next: `#next2`
-        }
-      });
+new Splide( '#slidewrapper', {
+	perPage: 3,
+	rewind : true,
+} ).mount();
